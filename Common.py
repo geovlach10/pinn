241db3260cfa694e391e966057ac67bbf8c8a651
geovlach10 <117597895+geovlach10@users.noreply.github.com>
2025-08-29 01:24:58 +0300

    Create Common.py

---
+++ Common.py
@@ -0,0 +1,93 @@
+import torch.nn as nn
+import torch
+import os
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+torch.manual_seed(42)
+
+
+class NeuralNet(nn.Module):
+
+    def __init__(self, input_dimension, output_dimension, n_hidden_layers, neurons, regularization_param, regularization_exp, retrain_seed):
+        super(NeuralNet, self).__init__()
+        # Number of input dimensions n
+        self.input_dimension = input_dimension
+        # Number of output dimensions m
+        self.output_dimension = output_dimension
+        # Number of neurons per layer
+        self.neurons = neurons
+        # Number of hidden layers
+        self.n_hidden_layers = n_hidden_layers
+        # Activation function
+        self.activation = nn.Tanh()
+        self.regularization_param = regularization_param
+        # Regularization exponent
+        self.regularization_exp = regularization_exp
+        # Random seed for weight initialization
+
+        self.input_layer = nn.Linear(self.input_dimension, self.neurons)
+        self.hidden_layers = nn.ModuleList([nn.Linear(self.neurons, self.neurons) for _ in range(n_hidden_layers - 1)])
+        self.output_layer = nn.Linear(self.neurons, self.output_dimension)
+        self.retrain_seed = retrain_seed
+        # Random Seed for weight initialization
+        self.init_xavier()
+
+    def forward(self, x):
+        # The forward function performs the set of affine and non-linear transformations defining the network
+        # (see equation above)
+        x = self.activation(self.input_layer(x))
+        for k, l in enumerate(self.hidden_layers):
+            x = self.activation(l(x))
+        return self.output_layer(x)
+
+    def init_xavier(self):
+        torch.manual_seed(self.retrain_seed)
+
+        def init_weights(m):
+            if type(m) == nn.Linear and m.weight.requires_grad and m.bias.requires_grad:
+                g = nn.init.calculate_gain('tanh')
+                torch.nn.init.xavier_uniform_(m.weight, gain=g)
+                # torch.nn.init.xavier_normal_(m.weight, gain=g)
+                m.bias.data.fill_(0)
+
+        self.apply(init_weights)
+
+    def regularization(self):
+        reg_loss = 0
+        for name, param in self.named_parameters():
+            if 'weight' in name:
+                reg_loss = reg_loss + torch.norm(param, self.regularization_exp)
+        return self.regularization_param * reg_loss
+
+
+def fit(model, training_set, num_epochs, optimizer, p, verbose=True):
+    history = list()
+
+    # Loop over epochs
+    for epoch in range(num_epochs):
+        if verbose: print("################################ ", epoch, " ################################")
+
+        running_loss = list([0])
+
+        # Loop over batches
+        for j, (x_train_, u_train_) in enumerate(training_set):
+            def closure():
+                # zero the parameter gradients
+                optimizer.zero_grad()
+                # forward + backward + optimize
+                u_pred_ = model(x_train_)
+                # Item 1. below
+                loss = torch.mean((u_pred_.reshape(-1, ) - u_train_.reshape(-1, )) ** p) + model.regularization()
+                # Item 2. below
+                loss.backward()
+                # Compute average training loss over batches for the current epoch
+                running_loss[0] += loss.item()
+                return loss
+
+            # Item 3. below
+            optimizer.step(closure=closure)
+
+        if verbose: print('Loss: ', (running_loss[0] / len(training_set)))
+        history.append(running_loss[0])
+
+    return history
